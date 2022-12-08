099984e1ef525f9d5be47f419c3949798729a2fd
daxbrat <dax.dacosta@gmail.com>
2022-12-08 07:47:46 -0600

    create playRound function

--- script.js
+++ script.js
@@ -10,4 +10,30 @@ function getComputerChoice() {
   }
 }
 
-console.log(getComputerChoice());
+let computerSelection = getComputerChoice();
+computerSelection = computerSelection.toLocaleLowerCase();
+console.log(computerSelection);
+
+let playerSelection = prompt("Rock, Paper or Scissors?");
+playerSelection = playerSelection.toLowerCase();
+console.log(playerSelection);
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "Tie";
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "You Win! Rock beats Scissors";
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    return "You Lose! Paper beats Rock";
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "You Win! Paper beats Rock";
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return "You Lose! Scissors beats Paper";
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return "You Lose! Rock beats Scissors";
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "You Win! Scissors beats Paper";
+  }
+}
+
+console.log(playRound(playerSelection, computerSelection));
