15518cd191b8e5d0ba9d155dc3270db27a94eb2c
daxbrat <dax.dacosta@gmail.com>
2022-12-09 17:38:26 -0600

    improve gave function

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+let playerScore = 0;
+let computerScore = 0;
+let computerSelection = "";
+let playerSelection = "";
+
 function getComputerChoice() {
-  let randomNumber = Math.floor(Math.random() * 3 ) + 1;
+  let randomNumber = Math.floor(Math.random() * 3) + 1;
   if (randomNumber === 1) {
     return "Rock";
-    
   } else if (randomNumber === 2) {
     return "Paper";
   } else {
@@ -10,40 +14,40 @@ function getComputerChoice() {
   }
 }
 
-let computerSelection = getComputerChoice();
-computerSelection = computerSelection.toLocaleLowerCase();
-console.log(computerSelection);
-
-let playerSelection = prompt("Rock, Paper or Scissors?");
-playerSelection = playerSelection.toLowerCase();
-console.log(playerSelection);
-
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     return "Tie";
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    playerScore++;
     return "You Win! Rock beats Scissors";
   } else if (playerSelection === "rock" && computerSelection === "paper") {
+    computerScore++;
     return "You Lose! Paper beats Rock";
   } else if (playerSelection === "paper" && computerSelection === "rock") {
+    playerScore++;
     return "You Win! Paper beats Rock";
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    computerScore++;
     return "You Lose! Scissors beats Paper";
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    computerScore++;
     return "You Lose! Rock beats Scissors";
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    playerScore++;
     return "You Win! Scissors beats Paper";
   }
 }
 
-
-
 function game() {
-  let playerScore = 0;
-  let computerScore = 0;
   for (let i = 0; i < 5; i++) {
-    console.log(playRound(playerSelection, computerSelection)); 
+    computerSelection = getComputerChoice();
+    computerSelection = computerSelection.toLocaleLowerCase();
+    console.log(computerSelection);
+
+    playerSelection = prompt("Rock, Paper or Scissors?");
+    playerSelection = playerSelection.toLowerCase();
+    console.log(playerSelection);
+
+    console.log(playRound(playerSelection, computerSelection));
   }
 }
-
-
